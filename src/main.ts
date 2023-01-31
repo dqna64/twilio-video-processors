0e69fe6a8064d03d3937ed8db4cd8c36690cba58
Gordon Huang <dqna64@gmail.com>
2023-02-01 00:55:33 +1100

    convert to ts

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,19 @@ import './style.css'
 import * as Video from 'twilio-video'
 // const { GaussianBlurBackgroundProcessor, VirtualBackgroundProcessor, isSupported } = Twilio.VideoProcessors;
 import { GaussianBlurBackgroundProcessor, VirtualBackgroundProcessor, isSupported } from '@twilio/video-processors';
-const bootstrap = window.bootstrap;
+// const bootstrap = window.bootstrap;
+import * as bootstrap from 'bootstrap'
 
-const gaussianBlurForm = document.querySelector('form#gaussianBlur-Form');
-const gaussianBlurButton = document.querySelector('button#gaussianBlur-Apply');
-const virtualBackgroundForm = document.querySelector('form#virtualBackground-Form');
-const virtualBackgroundButton = document.querySelector('button#virtualBackground-Apply');
-const videoInput = document.querySelector('video#video-input');
-const removeProcessorButton = document.querySelector('button#no-processor-apply');
-const errorMessage = document.querySelector('div.modal-body');
-const errorModal = new bootstrap.Modal(document.querySelector('div#errorModal'));
+const gaussianBlurForm: HTMLFormElement | null = document.querySelector('form#gaussianBlur-Form');
+const gaussianBlurButton: HTMLButtonElement | null = document.querySelector('button#gaussianBlur-Apply');
+const virtualBackgroundForm: HTMLFormElement | null = document.querySelector('form#virtualBackground-Form');
+const virtualBackgroundButton: HTMLButtonElement | null = document.querySelector('button#virtualBackground-Apply');
+const videoInput: HTMLVideoElement | null = document.querySelector('video#video-input');
+const removeProcessorButton: HTMLButtonElement | null = document.querySelector('button#no-processor-apply');
+const errorMessage: HTMLDivElement | null = document.querySelector('div.modal-body');
+const errorModelElement: HTMLDivElement | null = document.querySelector('div#errorModal')
+let errorModal: bootstrap.Modal | undefined
+if (errorModelElement) { errorModal = new bootstrap.Modal(errorModelElement); }
 
 // Same directory as the current js file
 const assetsPath = '';
@@ -23,24 +26,25 @@ let videoTrack: Video.LocalVideoTrack;
 let gaussianBlurProcessor: GaussianBlurBackgroundProcessor;
 let virtualBackgroundProcessor: VirtualBackgroundProcessor;
 
-if (!isSupported) {
-  errorMessage.textContent = 'This browser is not supported.';
+if (!isSupported && errorModal) {
+  if (errorMessage) errorMessage.textContent = 'This browser is not supported.';
   errorModal.show();
 }
 
-const loadImage = (name) =>
+const loadImage = (name: String) =>
   new Promise((resolve) => {
     const image = new Image();
-    image.src = `backgrounds/${name}.jpg`;
+    image.src = `./src/backgrounds/${name}.jpg`;
     image.onload = () => resolve(image);
   });
 
-let images = {};
+let images: any = {};
 Promise.all([
   loadImage('living_room'),
   loadImage('office'),
   loadImage('vacation'),
 ]).then(([livingRoom, office, vacation]) => {
+
   images.livingRoom = livingRoom;
   images.office = office;
   images.vacation = vacation;
@@ -52,73 +56,86 @@ Video.createLocalVideoTrack({
   height: 720,
   frameRate: 24,
 }).then((track) => {
-  track.attach(videoInput);
+  if (videoInput) track.attach(videoInput);
   return videoTrack = track;
 });
 
 // Adding processor to Video Track
-const setProcessor = (processor, track) => {
-  if (track.processor) {
+const setProcessor = (processor: Video.VideoProcessor | null, track: Video.LocalVideoTrack) => {
+  if (track.processor && removeProcessorButton) {
     removeProcessorButton.disabled = true;
     track.removeProcessor(track.processor);
   }
-  if (processor) {
+  if (processor && removeProcessorButton) {
     removeProcessorButton.disabled = false;
     track.addProcessor(processor);
   }
 };
 
-gaussianBlurButton.onclick = async event => {
-  event.preventDefault();
-  const options = {};
-  const inputs = gaussianBlurForm.getElementsByTagName('input');
-  for (let item of inputs) {
-    options[item.id] = item.valueAsNumber;
-  }
-  const { maskBlurRadius, blurFilterRadius } = options;
-  if (!gaussianBlurProcessor) {
-    gaussianBlurProcessor = new GaussianBlurBackgroundProcessor({
-      assetsPath,
-      maskBlurRadius,
-      blurFilterRadius,
-    });
-    await gaussianBlurProcessor.loadModel();
-  } else {
-    gaussianBlurProcessor.maskBlurRadius = maskBlurRadius;
-    gaussianBlurProcessor.blurFilterRadius = blurFilterRadius;
-  }
-  setProcessor(gaussianBlurProcessor, videoTrack);
-};
+if (gaussianBlurButton && gaussianBlurForm) {
+  gaussianBlurButton.onclick = async event => {
+    event.preventDefault();
+    const options: any = {};
+    const inputs = gaussianBlurForm.getElementsByTagName('input');
+    for (let item of Array.from(inputs)) {
+      options[item.id] = item.valueAsNumber;
+    }
+    const { maskBlurRadius, blurFilterRadius } = options;
+    if (!gaussianBlurProcessor) {
+      gaussianBlurProcessor = new GaussianBlurBackgroundProcessor({
+        assetsPath,
+        maskBlurRadius,
+        blurFilterRadius,
+      });
+      await gaussianBlurProcessor.loadModel();
+    } else {
+      gaussianBlurProcessor.maskBlurRadius = maskBlurRadius;
+      gaussianBlurProcessor.blurFilterRadius = blurFilterRadius;
+    }
+    setProcessor(gaussianBlurProcessor, videoTrack);
+  };
+}
 
-virtualBackgroundButton.onclick = async event => {
-  event.preventDefault();
-  const options = {};
-  const inputs = virtualBackgroundForm.elements;
-  for (let item of inputs) {
-    item.valueAsNumber
-      ? (options[item.id] = item.valueAsNumber)
-      : (options[item.id] = item.value);
-  }
-  let backgroundImage = images[options.backgroundImage];
-  let { maskBlurRadius, fitType } = options;
-  if (!virtualBackgroundProcessor) {
-    virtualBackgroundProcessor = new VirtualBackgroundProcessor({
-      assetsPath,
-      maskBlurRadius,
-      backgroundImage,
-      fitType,
-    });
-    await virtualBackgroundProcessor.loadModel();
-  } else {
-    virtualBackgroundProcessor.backgroundImage = backgroundImage;
-    virtualBackgroundProcessor.fitType = fitType;
-    virtualBackgroundProcessor.maskBlurRadius = maskBlurRadius;
-  }
-  setProcessor(virtualBackgroundProcessor, videoTrack);
-};
+if (virtualBackgroundButton && virtualBackgroundForm) {
+  virtualBackgroundButton.onclick = async event => {
+    event.preventDefault();
+    const options: any = {};
+    const inputs = virtualBackgroundForm.elements;
+    // for (let item of Array.from(inputs)) {
+    //   item.valueAsNumber
+    //     ? (options[item.id] = item.valueAsNumber)
+    //     : (options[item.id] = item.value);
+    // }
+    const itemVacation: any = inputs.namedItem('vacation')
+    if (itemVacation) {
+      itemVacation.valueAsNumber ? (options[itemVacation.id] = itemVacation.valueAsNumber) : options[itemVacation.id] = itemVacation.value
+    }
 
-removeProcessorButton.disabled = true;
-removeProcessorButton.onclick = event => {
-  event.preventDefault();
-  setProcessor(null, videoTrack);
-};
+    let backgroundImage = images[options.backgroundImage];
+
+    let { maskBlurRadius, fitType } = options;
+    if (!virtualBackgroundProcessor) {
+      virtualBackgroundProcessor = new VirtualBackgroundProcessor({
+        assetsPath,
+        maskBlurRadius,
+        backgroundImage,
+        fitType,
+      });
+      await virtualBackgroundProcessor.loadModel();
+    } else {
+      virtualBackgroundProcessor.backgroundImage = backgroundImage;
+      virtualBackgroundProcessor.fitType = fitType;
+      virtualBackgroundProcessor.maskBlurRadius = maskBlurRadius;
+    }
+    setProcessor(virtualBackgroundProcessor, videoTrack);
+  };
+}
+
+if (removeProcessorButton) {
+  removeProcessorButton.disabled = true;
+  removeProcessorButton.onclick = event => {
+    event.preventDefault();
+    setProcessor(null, videoTrack);
+  };
+
+}
